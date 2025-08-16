7dc9b77cb42e5cd1d4db7680ea622ff244263767
Zac Healy <zac.healy@CGI.com>
2025-08-16 09:04:37 +0100

    Added login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: '**', redirectTo: 'login' },
+];
