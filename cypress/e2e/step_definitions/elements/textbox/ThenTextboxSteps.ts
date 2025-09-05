24f5e930183b79603528d6618ac850671c55819e
Preksha <preksha.jain1@hmcts.net>
2025-09-05 10:20:37 +0100

    Screenshots added

--- cypress/e2e/step_definitions/elements/textbox/ThenTextboxSteps.ts
+++ cypress/e2e/step_definitions/elements/textbox/ThenTextboxSteps.ts
@@ -6,19 +6,19 @@ Then(
   'User enters {string} into the {string} textbox',
   (value: string, field: string) => {
     TextboxHelper.typeInTextbox(field, value);
-  },
-);
+    cy.screenshot(`TypedInTextbox-${field}-${value}`);
+  });
 
 Then('User clears the {string} textbox', (field: string) => {
   TextboxHelper.clearTextbox(field);
+  cy.screenshot(`ClearedTextbox-${field}`);
 });
 
 Then(
   'User verifies the {string} textbox contains {string}',
   (field: string, value: string) => {
     TextboxHelper.getValueInTextbox(field).should('eq', value);
-  },
-);
+  });
 
 Then('User verifies the {string} textbox is empty', (field: string) => {
   TextboxHelper.getValueInTextbox(field).should('be.empty');
