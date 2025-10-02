77677cb610e7f07a1d0ec90cdfb8f0a8ff49e33d
jason.nghiem <jason.nghiem@cgi.com>
2025-10-02 15:29:53 +0100

    fix unit test

--- test/unit/app/pages/applications-list-create.spec.ts/applications-list-create.spec.ts
+++ test/unit/app/pages/applications-list-create.spec.ts/applications-list-create.spec.ts
@@ -1,67 +1,71 @@
-import { PLATFORM_ID, TransferState } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+// test/unit/app/pages/applications-list-create.spec.ts/applications-list-create.spec.ts
+import { NO_ERRORS_SCHEMA, PLATFORM_ID, TransferState } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, provideRouter } from '@angular/router';
 import { of } from 'rxjs';
 
 import { ApplicationsListCreate } from '../../../../../src/app/pages/applications-list-create/applications-list-create';
 import {
-  CourtLocationPage,
+  ApplicationListsApi,
   CourtLocationsApi,
-  CriminalJusticeAreaPage,
   CriminalJusticeAreasApi,
 } from '../../../../../src/generated/openapi';
 
-class CourtLocationsApiStub {
-  getCourtLocations(..._args: unknown[]) {
-    void _args;
-    const page: CourtLocationPage = {
-      content: [],
-      pageNumber: 0,
-      pageSize: 0,
-      totalElements: 0,
-      elementsOnPage: 0,
-    };
-    return of(page);
-  }
-}
-class CriminalJusticeAreasApiStub {
-  getCriminalJusticeAreas(..._args: unknown[]) {
-    void _args;
-    const page: CriminalJusticeAreaPage = {
-      content: [],
-      pageNumber: 0,
-      pageSize: 0,
-      totalElements: 0,
-      elementsOnPage: 0,
-    };
-    return of(page);
-  }
-}
+describe('ApplicationsListCreate', () => {
+  let fixture: ComponentFixture<ApplicationsListCreate>;
 
-const activatedRouteStub = {
-  snapshot: { data: { nationalCourtHouses: [] } },
-  params: of({}),
-  queryParams: of({}),
-};
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ApplicationsListCreate],
+      providers: [
+        provideRouter([]),
+        { provide: PLATFORM_ID, useValue: 'browser' },
+        TransferState,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { data: { nationalCourtHouses: [] } },
+            params: of({}),
+            queryParams: of({}),
+          },
+        },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    })
+      // Avoid HttpClient by stubbing OpenAPI services with plain objects
+      .overrideProvider(ApplicationListsApi, {
+        useValue: { createApplicationList: () => of({ id: 123 }) },
+      })
+      .overrideProvider(CourtLocationsApi, {
+        useValue: {
+          getCourtLocations: () =>
+            of({
+              content: [],
+              pageNumber: 0,
+              pageSize: 0,
+              totalElements: 0,
+              elementsOnPage: 0,
+            }),
+        },
+      })
+      .overrideProvider(CriminalJusticeAreasApi, {
+        useValue: {
+          getCriminalJusticeAreas: () =>
+            of({
+              content: [],
+              pageNumber: 0,
+              pageSize: 0,
+              totalElements: 0,
+              elementsOnPage: 0,
+            }),
+        },
+      })
+      .compileComponents();
 
-beforeEach(async () => {
-  await TestBed.configureTestingModule({
-    imports: [ApplicationsListCreate],
-    providers: [
-      provideRouter([]),
-      { provide: ActivatedRoute, useValue: activatedRouteStub },
-      TransferState,
-      { provide: PLATFORM_ID, useValue: 'browser' },
-      { provide: CourtLocationsApi, useClass: CourtLocationsApiStub },
-      {
-        provide: CriminalJusticeAreasApi,
-        useClass: CriminalJusticeAreasApiStub,
-      },
-    ],
-  }).compileComponents();
-});
+    fixture = TestBed.createComponent(ApplicationsListCreate);
+  });
 
-it('should create', () => {
-  const fixture = TestBed.createComponent(ApplicationsListCreate);
-  expect(fixture.componentInstance).toBeTruthy();
+  it('should create', () => {
+    expect(fixture.componentInstance).toBeTruthy();
+  });
 });
