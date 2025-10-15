26d5212fbc60918cd2119f3ada444c99fc9f21c3
jason.nghiem <jason.nghiem@cgi.com>
2025-10-15 10:04:12 +0100

    revert unnecessary change.

--- cypress.config.js
+++ cypress.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
     fixturesFolder: 'cypress/fixtures',
 
     // Base URL and Timeouts
-    baseUrl: process.env.TEST_URL || 'https://appreg.staging.apps.hmcts.net',
+    baseUrl: process.env.TEST_URL || 'http://localhost:4000',
     defaultCommandTimeout: 20000,
     pageLoadTimeout: 120000,
     requestTimeout: 20000,
@@ -141,7 +141,7 @@ module.exports = defineConfig({
       framework: 'angular',
       bundler: 'webpack',
     },
-    baseUrl: process.env.TEST_URL || 'https://appreg.staging.apps.hmcts.net',
+    baseUrl: process.env.TEST_URL || 'http://localhost:4000',
     // Component Test Report Configuration
     reporter: 'cypress-multi-reporters',
     reporterOptions: {
