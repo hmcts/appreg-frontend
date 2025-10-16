9a47a023097112287eeaae2180c20258eea60155
jason-j-nghiem <Jason.Nghiem@HMCTS.NET>
2025-10-16 14:47:43 +0100

    Stg (Attempt: try to load properties-volume first first to ensure the config is picked up)(#182)
    * try to load pv before config, dynamically load config afterwards
    
    * Add custom-environment-variables.json
    
    * try preloading pv
    
    * lint
    
    ---------
    
    Co-authored-by: jason.nghiem <jason.nghiem@cgi.com>

---
+++ pv-preload.cjs
@@ -0,0 +1,22 @@
+'use strict';
+
+const path = require('node:path');
+
+process.env.NODE_CONFIG_DIR =
+  process.env.NODE_CONFIG_DIR || path.resolve(process.cwd(), 'config');
+process.env.PV_PRELOADED = '1';
+
+const config = require('config');
+
+try {
+  const pvm = require('@hmcts/properties-volume');
+  const _ = require('lodash');
+  pvm.addTo(config);
+  const aik = _.get(config, 'secrets.rpe.AppInsightsInstrumentationKey');
+  if (aik) {
+    _.set(config, 'appInsights.instrumentationKey', aik);
+  }
+} catch (_err) {
+  // ignore
+  void _err;
+}
