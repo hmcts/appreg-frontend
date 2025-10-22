debf5ad51d343f2e48854c8f4c61251c566f7dce
Zac Healy <zac.healy@CGI.com>
2025-10-22 12:55:23 +0100

    Breaking dependency on development.json

--- src/modules/properties-volume/index.ts
+++ src/modules/properties-volume/index.ts
@@ -1,6 +1,7 @@
 import { createRequire } from 'module';
 
 import type * as PV from '@hmcts/properties-volume';
+import { addFromAzureVault } from '@hmcts/properties-volume';
 import { IConfig } from 'config';
 import { Application } from 'express';
 
@@ -8,38 +9,25 @@ const require = createRequire(import.meta.url);
 
 export class PropertiesVolume {
   async enableFor(server: Application): Promise<void> {
+
+    if (process.env['NODE_ENV'] === 'development') {
+      const { default: config } = (await import('config')) as {
+        default: IConfig;
+      };
+
+      await addFromAzureVault(config, {
+        pathToHelmChart: 'charts/appreg-frontend/values.yaml', env: 'stg'
+      });
+    }
+
     if (server.locals['ENV'] !== 'development') {
       const { default: config } = (await import('config')) as {
         default: IConfig;
       };
 
       const pvm = require('@hmcts/properties-volume') as typeof PV;
-      const { get, set } = require('lodash') as {
-        get: (typeof import('lodash'))['get'];
-        set: (typeof import('lodash'))['set'];
-      };
 
       pvm.addTo(config);
-      this.setSecret(
-        config,
-        get,
-        set,
-        'secrets.rpe.AppInsightsInstrumentationKey',
-        'appInsights.instrumentationKey',
-      );
-    }
-  }
-
-  private setSecret(
-    config: IConfig,
-    get: <T>(obj: unknown, path: string) => T,
-    set: (obj: unknown, path: string, value: unknown) => void,
-    fromPath: string,
-    toPath: string,
-  ): void {
-    if (config.has(fromPath)) {
-      const value = config.get<unknown>(fromPath);
-      set(config, toPath, value);
     }
   }
 }
