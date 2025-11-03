38ef5aee90887e8087aac6b9d9af3c82746bc7c2
jason.nghiem <jason.nghiem@cgi.com>
2025-11-03 15:16:22 +0000

    Fix button

--- src/app/pages/applications-list-entry-create/applications-list-entry-create.ts
+++ src/app/pages/applications-list-entry-create/applications-list-entry-create.ts
@@ -1,3 +1,13 @@
+/* 
+Applications List Entry – Create (/applications-list/:id/create)
+
+Functionality:
+  - Creates application list entry payload
+    - Validate against DTO
+    - Conform valid data to existing types/DTOs
+  - Run POST query with payload
+*/
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -17,6 +27,15 @@ export class ApplicationsListEntryCreate implements OnInit {
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id')!;
   }
+
+  onSubmit(e: Event): void {
+    e.preventDefault();
+
+    // TODO: run post request
+    // console.log('submit happened');
+  }
+
+  
 
   buildReturnLink(): { label: string; link: string } {
     return { label: 'Back to list', link: `/applications-list/${this.id}` };
