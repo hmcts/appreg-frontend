7eb3325aff8f50f2b3b92fdf5981430cf1447b46
jason.nghiem <jason.nghiem@cgi.com>
2025-11-04 09:48:43 +0000

    radio button component added, applicant selection screen added

--- src/app/pages/applications-list-entry-create/applications-list-entry-create.ts
+++ src/app/pages/applications-list-entry-create/applications-list-entry-create.ts
@@ -9,20 +9,44 @@ Functionality:
 */
 
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 import { BreadcrumbsComponent } from '../../shared/components/breadcrumbs/breadcrumbs.component';
+import { RadioButtonComponent } from '../../shared/components/radio-button/radio-button.component';
+
+type ApplicantStep = 'select' | 'person' | 'org' | 'standard';
 
 @Component({
   selector: 'app-applications-list-entry-create',
   standalone: true,
-  imports: [BreadcrumbsComponent],
+  imports: [
+    BreadcrumbsComponent,
+    RadioButtonComponent,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './applications-list-entry-create.html',
 })
 export class ApplicationsListEntryCreate implements OnInit {
   id: string = '';
+  step: ApplicantStep = 'select';
+
+  submitted: boolean = false;
 
   constructor(private readonly route: ActivatedRoute) {}
+
+  form = new FormGroup({
+    applicant: new FormControl<string | null>('org', {
+      nonNullable: false,
+    }), // Organisation is set as default (same as old app)
+  });
+
+  applicantOptions = [
+    { label: 'Person', value: 'person' },
+    { label: 'Organisation', value: 'org' },
+    { label: 'Standard Applicant', value: 'standard' },
+  ];
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id')!;
@@ -35,7 +59,21 @@ export class ApplicationsListEntryCreate implements OnInit {
     // console.log('submit happened');
   }
 
-  
+  onNext(): void {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    const v = this.form.get('applicant')!.value as Exclude<
+      ApplicantStep,
+      'select'
+    >;
+    this.step = v; // 'person' | 'org' | 'standard'
+  }
+
+  onBack(): void {
+    this.step = 'select';
+  }
 
   buildReturnLink(): { label: string; link: string } {
     return { label: 'Back to list', link: `/applications-list/${this.id}` };
