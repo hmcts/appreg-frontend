51aa5dbde84a60328122512b1ce03c861fcfe8c6
Zac Healy <zac.healy@CGI.com>
2025-11-06 11:24:36 +0000

    Replacing raw HTML with components

--- src/app/pages/applications-list-entry-detail/applications-list-entry-detail.ts
+++ src/app/pages/applications-list-entry-detail/applications-list-entry-detail.ts
@@ -1,11 +1,13 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 import { AccordionComponent } from '../../shared/components/accordion/accordion.component';
 import { BreadcrumbsComponent } from '../../shared/components/breadcrumbs/breadcrumbs.component';
+import { SelectInputComponent } from '../../shared/components/select-input/select-input.component';
 import { SortableTableComponent, TableColumn } from '../../shared/components/sortable-table/sortable-table.component';
+import { TextInputComponent } from '../../shared/components/text-input/text-input.component';
 import { MojButtonMenuDirective } from '../../shared/util/moj-button-menu';
 
 @Component({
@@ -19,32 +21,50 @@ import { MojButtonMenuDirective } from '../../shared/util/moj-button-menu';
     AccordionComponent,
     MojButtonMenuDirective,
     SortableTableComponent,
+    SelectInputComponent,
+    TextInputComponent,
   ],
   templateUrl: './applications-list-entry-detail.html',
 })
 export class ApplicationsListEntryDetail implements OnInit {
   appListId!: string;
   formSubmitted = false;
+  form!: FormGroup;
   entryType = new FormControl<'Person' | 'Organisation' | 'Standard Applicant'>(
     'Organisation',
   );
-  personOpen = false;
 
   columns: TableColumn[] = [
-    { header: 'Code', field: 'code', numeric: true  },
+    { header: 'Code', field: 'code', numeric: true },
     { header: 'Name', field: 'name' },
     { header: 'Address line 1', field: 'address' },
     { header: 'Use from', field: 'useFrom' },
     { header: 'Use to', field: 'useTo' },
   ];
 
+  entryTypeOptions = [
+    { value: 'Person', label: 'Person' },
+    { value: 'Organisation', label: 'Organisation' },
+    { value: 'Standard Applicant', label: 'Standard Applicant' },
+  ];
+
+  personTitleOptions = [
+    { value: 'Mr', label: 'Mr' },
+    { value: 'Mrs', label: 'Mrs' },
+    { value: 'Miss', label: 'Miss' },
+    { value: 'Dr', label: 'Dr' },
+    { value: 'Other', label: 'Other' },
+  ];
+
   constructor(
+    private readonly fb: FormBuilder,
     @Inject(PLATFORM_ID) private readonly platformId: object,
     private readonly route: ActivatedRoute,
     private readonly router: Router,
   ) {}
 
   ngOnInit(): void {
+    // existing appListId resolution (unchanged)
     const nav = this.router.currentNavigation();
     const fromNav = nav?.extras?.state as { appListId?: string } | undefined;
     const fromHist = isPlatformBrowser(this.platformId)
@@ -57,10 +77,35 @@ export class ApplicationsListEntryDetail implements OnInit {
       this.route.snapshot.queryParamMap.get('appListId') ??
       '';
 
-    this.entryType.valueChanges.subscribe((v) => {
-      if (v !== 'Person') {
-        this.personOpen = false;
-      }
+    // NEW: build the form
+    this.form = this.fb.group({
+      entryType: ['Organisation'],
+      person: this.fb.group({
+        title: [''],
+        firstName: [''],
+        middleNames: [''],
+        surname: [''],
+        addressLine1: [''],
+        addressLine2: [''],
+        addressLine3: [''],
+        addressLine4: [''],
+        addressLine5: [''],
+        postcode: [''],
+        phoneNumber: [''],
+        mobileNumber: [''],
+        emailAddress: [''],
+      }),
+      organisation: this.fb.group({
+        name: [''],
+        addressLine1: [''],
+        addressLine2: [''],
+        addressLine3: [''],
+        addressLine4: [''],
+        addressLine5: [''],
+        postcode: [''],
+        phoneNumber: [''],
+        emailAddress: [''],
+      }),
     });
   }
 
