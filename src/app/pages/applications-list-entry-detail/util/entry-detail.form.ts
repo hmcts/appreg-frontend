0082e3df4ff8aed63c76990c9049a9d2fbc586f8
Zac Healy <zac.healy@CGI.com>
2025-11-13 09:58:00 +0000

    Added unit tests for application list entry detail

--- src/app/pages/applications-list-entry-detail/util/entry-detail.form.ts
+++ src/app/pages/applications-list-entry-detail/util/entry-detail.form.ts
@@ -1,45 +1,76 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormGroup, NonNullableFormBuilder } from '@angular/forms';
 
-export function buildEntryDetailForm(fb: FormBuilder): FormGroup {
+export function buildEntryDetailForm(fb: NonNullableFormBuilder): FormGroup {
   return fb.group({
-    lodgementDate: [{ value: '', disabled: true }],
-    applicationCode: [''],
-    applicationTitle: [''],
+    // Codes section
+    lodgementDate: fb.control({ value: '', disabled: true }),
+    applicationCode: fb.control(''),
+    applicationTitle: fb.control(''),
 
-    applicantEntryType: ['Organisation'],
-    respondentEntryType: ['Organisation'],
+    // Entry type selectors (lowercase to match @switch cases in template)
+    applicantEntryType: fb.control<'person' | 'organisation' | 'standardApplicant'>('organisation'),
+    respondentEntryType: fb.control<'person' | 'organisation'>('organisation'),
 
+    // Wording section
+    courtName: fb.control(''),
+    organisationName: fb.control(''),
+
+    // Civil fee section
+    feeStatus: fb.control(''),
+    feeStatusDate: fb.control(''),
+    paymentRef: fb.control(''),
+
+    // Notes section
+    caseReference: fb.control(''),
+    accountReference: fb.control(''),
+    applicationDetails: fb.control(''),
+
+    // Result wording section
+    resultCode: fb.control(''),
+
+    // Person sub-group
     person: fb.group({
-      title: [''],
-      firstName: [''],
-      middleNames: [''],
-      surname: [''],
-      addressLine1: [''],
-      addressLine2: [''],
-      addressLine3: [''],
-      addressLine4: [''],
-      addressLine5: [''],
-      postcode: [''],
-      phoneNumber: [''],
-      mobileNumber: [''],
-      emailAddress: [''],
-    }),
-    organisation: fb.group({
-      name: [''],
-      addressLine1: [''],
-      addressLine2: [''],
-      addressLine3: [''],
-      addressLine4: [''],
-      addressLine5: [''],
-      postcode: [''],
-      phoneNumber: [''],
-      emailAddress: [''],
+      title: fb.control(''),
+      firstName: fb.control(''),
+      middleNames: fb.control(''),
+      surname: fb.control(''),
+      addressLine1: fb.control(''),
+      addressLine2: fb.control(''),
+      addressLine3: fb.control(''),
+      addressLine4: fb.control(''),
+      addressLine5: fb.control(''),
+      postcode: fb.control(''),
+      phoneNumber: fb.control(''),
+      mobileNumber: fb.control(''),
+      emailAddress: fb.control(''),
     }),
 
-    // (keep the rest exactly as you had them)
-    mags1Title: [''], mags1FirstName: [''], mags1Surname: [''],
-    mags2Title: [''], mags2FirstName: [''], mags2Surname: [''],
-    mags3Title: [''], mags3FirstName: [''], mags3Surname: [''],
-    officialTitle: [''], officialFirstName: [''], officialSurname: [''],
+    // Organisation sub-group
+    organisation: fb.group({
+      name: fb.control(''),
+      addressLine1: fb.control(''),
+      addressLine2: fb.control(''),
+      addressLine3: fb.control(''),
+      addressLine4: fb.control(''),
+      addressLine5: fb.control(''),
+      postcode: fb.control(''),
+      phoneNumber: fb.control(''),
+      mobileNumber: fb.control(''),
+      emailAddress: fb.control(''),
+    }),
+
+    // Officials section
+    mags1Title: fb.control(''),
+    mags1FirstName: fb.control(''),
+    mags1Surname: fb.control(''),
+    mags2Title: fb.control(''),
+    mags2FirstName: fb.control(''),
+    mags2Surname: fb.control(''),
+    mags3Title: fb.control(''),
+    mags3FirstName: fb.control(''),
+    mags3Surname: fb.control(''),
+    officialTitle: fb.control(''),
+    officialFirstName: fb.control(''),
+    officialSurname: fb.control(''),
   });
 }
