5b739a5f3f11d9bcb9596c3b0c9998fad5084be8
Tvisha <tvisha.pandya1@hmcts.com>
2025-11-17 09:32:25 +0000

    Changed the approach for "No records found" in TextboxHelper.ts file. (bug ARCPOC-687)

--- cypress/support/helper/forms/textbox/TextboxHelper.ts
+++ cypress/support/helper/forms/textbox/TextboxHelper.ts
@@ -98,35 +98,30 @@ export class TextboxHelper {
   /**
    * Verifies that the "No results found" message is visible in the autocomplete menu
    */
-  static verifyAutocompleteNoResultsVisible(
-    _selector: string,
-    info: string,
-  ): Cypress.Chainable {
-    return cy
-      .get('.app-autocomplete__menu')
-      .should('be.visible')
-      .contains('.app-autocomplete__menu', info, {
-        matchCase: false,
-      })
-      .should('be.visible');
+  static verifyInfoVisible(selector: string, info: string): Cypress.Chainable {
+    {
+      return TextboxElement.findTextbox(selector).then(($el) => {
+        {
+          return cy
+            .get('.app-autocomplete__menu:visible')
+            .should('contain.text', info);
+        }
+      });
+    }
   }
-
   /**
    * Verifies that the "No results found" message is not visible (menu hidden or message absent)
    */
-  static verifyAutocompleteNoResultsNotVisible(
+  static verifyInfoNotVisible(
     selector: string,
     info: string,
   ): Cypress.Chainable {
     return TextboxElement.findTextbox(selector).then(($el) => {
-      // If the menu is present and visible, assert it does NOT contain the message.
-      if ($el.find('.app-autocomplete__menu:visible').length) {
-        return cy
-          .get('.app-autocomplete__menu:visible')
-          .should('not.contain.text', info);
-      }
-      // Menu not present or not visible -> treat as not visible
-      return cy.wrap(null);
+      if ($el.find('.app-autocomplete__menu').length === 0) {
+        cy.log(info, 'is not visible');
+        return cy.get('.app-autocomplete__menu').should('not.exist');
+      } else
+        return cy.get('.app-autocomplete__menu').should('contain.text', info);
     });
   }
 }
