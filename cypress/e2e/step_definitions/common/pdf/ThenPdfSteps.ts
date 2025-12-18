24c6d9015fd7a4a013f546d9885c5bf843370596
Preksha Jain <preksha.jain1@hmcts.net>
2025-12-18 14:24:21 +0000

    PDF code refactor

--- cypress/e2e/step_definitions/common/pdf/ThenPdfSteps.ts
+++ cypress/e2e/step_definitions/common/pdf/ThenPdfSteps.ts
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 import { Then } from '@badeball/cypress-cucumber-preprocessor';
 
+import { PdfAssertions } from '../../../../support/helper/pdf/PdfAssertions';
 import { PdfDownloadHelper } from '../../../../support/helper/pdf/PdfDownloadHelper';
-import { StringUtils } from '../../../../support/utils/StringUtils';
 
 Then('User Clears Downloaded PDFs', () => {
   PdfDownloadHelper.clearDownloadsFolder();
@@ -17,115 +17,31 @@ Then('User Verifies PDF {string} Is Downloaded', (partialName: string) => {
 Then(
   'User Verifies Latest Downloaded PDF Contains Text {string}',
   (expectedText: string) => {
-    PdfDownloadHelper.listPdfFiles().then((files) => {
-      expect(
-        files.length,
-        `Expected at least 1 PDF file in downloads folder, but found ${files.length}`,
-      ).to.be.greaterThan(0);
-
-      const latestPdf = files[files.length - 1];
-      cy.log(`Verifying PDF contains text: "${expectedText}"`);
-      PdfDownloadHelper.verifyPdfContainsText(latestPdf, expectedText);
-    });
+    PdfAssertions.verifyLatestPdfContainsText(expectedText);
   },
 );
 
 Then(
   'User Verifies Latest Downloaded PDF Contains {int} {string} Entries',
   (expectedCount: number, entryType: string) => {
-    PdfDownloadHelper.listPdfFiles().then((files) => {
-      expect(
-        files.length,
-        `Expected at least 1 PDF file in downloads folder, but found ${files.length}`,
-      ).to.be.greaterThan(0);
-
-      const latestPdf = files[files.length - 1];
-      cy.log(`Verifying PDF has ${expectedCount} "${entryType}" entries`);
-
-      PdfDownloadHelper.getPdfText(latestPdf).then((text) => {
-        const escapedEntryType = entryType.replaceAll(
-          /[.*+?^${}()|[\]\\]/g,
-          String.raw`\$&`,
-        );
-        const pattern = new RegExp(
-          String.raw`\d+\.\s+${escapedEntryType}`,
-          'g',
-        );
-        const matches = text.match(pattern);
-        const actualCount = matches ? matches.length : 0;
-
-        expect(
-          actualCount,
-          `Expected ${expectedCount} "${entryType}" entries in PDF, but found ${actualCount}`,
-        ).to.equal(expectedCount);
-      });
-    });
+    PdfAssertions.verifyLatestPdfContainsEntries(expectedCount, entryType);
   },
 );
 
 Then('User Verifies Latest Downloaded PDF Is Not Empty', () => {
-  PdfDownloadHelper.listPdfFiles().then((files) => {
-    expect(
-      files.length,
-      `Expected at least 1 PDF file in downloads folder, but found ${files.length}`,
-    ).to.be.greaterThan(0);
-
-    const latestPdf = files[files.length - 1];
-    cy.log(`Verifying PDF is not empty: ${latestPdf}`);
-    PdfDownloadHelper.verifyFileNotEmpty(latestPdf);
-  });
+  PdfAssertions.verifyLatestPdfIsNotEmpty();
 });
 
 Then(
   'User Verifies Latest Downloaded PDF Has {int} Pages',
   (expectedPages: number) => {
-    PdfDownloadHelper.listPdfFiles().then((files) => {
-      expect(
-        files.length,
-        `Expected at least 1 PDF file in downloads folder, but found ${files.length}`,
-      ).to.be.greaterThan(0);
-
-      const latestPdf = files[files.length - 1];
-      cy.log(`Verifying PDF has ${expectedPages} page(s)`);
-      PdfDownloadHelper.verifyPdfPageCount(latestPdf, expectedPages);
-    });
+    PdfAssertions.verifyLatestPdfPageCount(expectedPages);
   },
 );
 
 Then(
   'User Verifies Latest Downloaded PDF Contains The Following Values:',
   (dataTable: { rawTable: string[][] }) => {
-    PdfDownloadHelper.listPdfFiles().then((files) => {
-      expect(
-        files.length,
-        `Expected at least 1 PDF file in downloads folder, but found ${files.length}`,
-      ).to.be.greaterThan(0);
-
-      const latestPdf = files[files.length - 1];
-      const rows = dataTable.rawTable;
-
-      cy.log(`Verifying PDF contains ${rows.length} values`);
-
-      PdfDownloadHelper.getPdfText(latestPdf).then((text) => {
-        const normalizedText = StringUtils.normalizeText(text);
-
-        for (const [key, value] of rows) {
-          cy.log(`Checking: ${key} = "${value}"`);
-
-          const normalizedKey = StringUtils.normalizeText(key);
-          const normalizedValue = StringUtils.normalizeText(value);
-
-          expect(normalizedText).to.include(
-            normalizedKey,
-            `PDF should contain key: "${key}"`,
-          );
-
-          expect(normalizedText).to.include(
-            normalizedValue,
-            `PDF should contain value: "${value}"`,
-          );
-        }
-      });
-    });
+    PdfAssertions.verifyLatestPdfContainsValues(dataTable.rawTable);
   },
 );
