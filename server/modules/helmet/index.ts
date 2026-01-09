c543866af6fb2e80055dc7597a517d351e007792
Jason Nghiem <Jason.Nghiem@HMCTS.NET>
2026-01-09 15:29:14 +0000

    Fix reinit, undo nonce changes

--- server/modules/helmet/index.ts
+++ server/modules/helmet/index.ts
@@ -1,13 +1,8 @@
-import crypto from 'node:crypto';
-import type { IncomingMessage, ServerResponse } from 'node:http';
-
 import * as express from 'express';
 import helmet from 'helmet';
 
 const self = "'self'";
 // Helmet expects (IncomingMessage, ServerResponse) so read nonce from res.locals
-const nonceDirective = (_req: IncomingMessage, res: ServerResponse): string =>
-  `'nonce-${(res as { locals?: Record<string, string> }).locals?.['cspNonce'] ?? ''}'`;
 
 /**
  * Module that enables helmet in the application
@@ -19,20 +14,14 @@ export class Helmet {
   }
 
   public enableFor(app: express.Express): void {
-    app.use((_req, res, next) => {
-      // Generate a per-response nonce for CSP so we can inject it into inline scripts
-      res.locals['cspNonce'] = crypto.randomBytes(16).toString('base64');
-      next();
-    });
-
-    const scriptSrc = [self, nonceDirective];
+    const scriptSrc = [self];
 
     if (this.developmentMode) {
       // Uncaught EvalError: Refused to evaluate a string as JavaScript because 'unsafe-eval'
       // is not an allowed source of script in the following Content Security Policy directive:
       // "script-src 'self' *.google-analytics.com 'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='".
       // seems to be related to webpack
-      scriptSrc.push("'unsafe-eval'", "'unsafe-inline'");
+      scriptSrc.push("'unsafe-eval'");
     }
 
     app.use(
