665f03fbe71d01ae11180e14860cdce563aeab2b
Tvisha <tvisha.pandya1@hmcts.com>
2026-02-18 16:22:10 +0000

    yarn lint:fix is run

--- cypress/e2e/step_definitions/elements/details/ThenDetailsSteps.ts
+++ cypress/e2e/step_definitions/elements/details/ThenDetailsSteps.ts
@@ -1,15 +1,24 @@
-import {Then} from "@badeball/cypress-cucumber-preprocessor";
+import { Then } from '@badeball/cypress-cucumber-preprocessor';
 
-import {DetailsHelper} from "../../../../support/helper/forms/details/DetailsHelper";
+import { DetailsHelper } from '../../../../support/helper/forms/details/DetailsHelper';
 
-Then('User Should See The Details section {string}', (detailsSectionTitle: string) => {
-  DetailsHelper.verifyDetailsSectionVisible(detailsSectionTitle);
-});
+Then(
+  'User Should See The Details section {string}',
+  (detailsSectionTitle: string) => {
+    DetailsHelper.verifyDetailsSectionVisible(detailsSectionTitle);
+  },
+);
 
-Then('User Should Not See The Details section {string}', (detailsSectionTitle: string) => {
-  DetailsHelper.verifyDetailsSectionNotVisible(detailsSectionTitle);
-});
+Then(
+  'User Should Not See The Details section {string}',
+  (detailsSectionTitle: string) => {
+    DetailsHelper.verifyDetailsSectionNotVisible(detailsSectionTitle);
+  },
+);
 
-Then('User Clicks On The Details section {string}', (detailsSectionTitle: string) => {
-  DetailsHelper.clickDetailsSection(detailsSectionTitle);
-});
+Then(
+  'User Clicks On The Details section {string}',
+  (detailsSectionTitle: string) => {
+    DetailsHelper.clickDetailsSection(detailsSectionTitle);
+  },
+);
