fc55f7338ca934270055092655fb737806183a38
Tvisha <tvisha.pandya1@hmcts.com>
2026-02-20 10:56:30 +0000

    return type modified

--- cypress/support/helper/forms/accordion/AccordionHelper.ts
+++ cypress/support/helper/forms/accordion/AccordionHelper.ts
@@ -1,21 +1,26 @@
 import { AccordionElement } from '../../../pageobjects/generic/accordion/AccordionElement';
 
 export class AccordionHelper {
-  static IsAccordionVisible(accordionTitle: string) {
-    AccordionElement.findAccordion(accordionTitle).should('be.visible');
+  /**
+   * Generic method to check if an accordion is visible by its title
+   */
+  static IsAccordionVisible(
+    detailsSectionTitle: string,
+  ): Cypress.Chainable<JQuery<HTMLElement>> {
+    return AccordionElement.findAccordion(detailsSectionTitle).should(
+      'be.visible',
+    );
   }
 
+  /**
+   * Generic method to verify if a textbox with a specific label is present within an accordion
+   */
   static verifyTextboxInAccordion(
-    textboxLabel: string,
-    accordionTitle: string,
-  ) {
-    AccordionElement.findTextboxInAccordion(
-      textboxLabel,
-      accordionTitle,
-    ).should('be.visible');
-  }
-
-  static toggleAccordion(accordionTitle: string) {
-    AccordionElement.findAccordion(accordionTitle).click();
+    labelText: string,
+    detailsSectionTitle: string,
+  ): Cypress.Chainable<JQuery<HTMLElement>> {
+    return AccordionElement.findTextboxInAccordion(labelText, detailsSectionTitle).should(
+      'be.visible',
+    );
   }
 }
