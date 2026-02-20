1821100cb466b32e3310f7bb52b8da3a182191fd
Tvisha <tvisha.pandya1@hmcts.com>
2026-02-20 10:41:37 +0000

    Implement accordion functionality and update application list entry component verification feature

---
+++ cypress/support/helper/forms/accordion/AccordionHelper.ts
@@ -0,0 +1,21 @@
+import { AccordionElement } from '../../../pageobjects/generic/accordion/AccordionElement';
+
+export class AccordionHelper {
+  static IsAccordionVisible(accordionTitle: string) {
+    AccordionElement.findAccordion(accordionTitle).should('be.visible');
+  }
+
+  static verifyTextboxInAccordion(
+    textboxLabel: string,
+    accordionTitle: string,
+  ) {
+    AccordionElement.findTextboxInAccordion(
+      textboxLabel,
+      accordionTitle,
+    ).should('be.visible');
+  }
+
+  static toggleAccordion(accordionTitle: string) {
+    AccordionElement.findAccordion(accordionTitle).click();
+  }
+}
