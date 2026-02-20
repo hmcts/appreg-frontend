4fd11273c6705a409714fad1b24992311d44df0f
Tvisha <tvisha.pandya1@hmcts.com>
2026-02-20 13:05:09 +0000

    Fix typo in findAccordion method and remove unused findTextboxInAccordion method

--- cypress/support/pageobjects/generic/accordion/AccordionElement.ts
+++ cypress/support/pageobjects/generic/accordion/AccordionElement.ts
@@ -4,20 +4,6 @@ export class AccordionElement {
   static findAccordion(
     accordionTitle: string,
   ): Cypress.Chainable<JQuery<HTMLElement>> {
-    return cy.contains('details summary, summary, detatils', accordionTitle);
-  }
-
-  static findTextboxInAccordion(
-    textboxLabel: string,
-    accordionTitle: string,
-  ): Cypress.Chainable<JQuery<HTMLElement>> {
-    return this.findAccordion(accordionTitle)
-      .should('be.visible')
-      .parent()
-      .within(() => {
-        cy.contains('label', textboxLabel, { matchCase: false }).should(
-          'exist',
-        );
-      });
+    return cy.contains('details summary, summary, details', accordionTitle);
   }
 }
