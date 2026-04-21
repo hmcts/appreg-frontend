aa19a147e7d773e142c0d522fa3ee881b3a7f1d1
Jason Nghiem <Jason.Nghiem@HMCTS.NET>
2026-04-21 11:06:45 +0100

    Clear search functionality

--- src/app/shared/components/applications-list-detail-search/applications-list-detail-search.component.ts
+++ src/app/shared/components/applications-list-detail-search/applications-list-detail-search.component.ts
@@ -1,9 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, input } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 import { SelectInputComponent } from '@components/select-input/select-input.component';
 import { TextInputComponent } from '@components/text-input/text-input.component';
+import { UK_POSTCODE_REGEX } from '@constants/regex';
 
 @Component({
   selector: 'app-applications-list-detail-search',
@@ -18,5 +24,31 @@ import { TextInputComponent } from '@components/text-input/text-input.component'
   styleUrl: './applications-list-detail-search.component.scss',
 })
 export class ApplicationsListDetailSearchComponent {
-  form = input<FormGroup>();
+  form = new FormGroup({
+    sequenceNumber: new FormControl<string>(''),
+    accountNumber: new FormControl<string>(''),
+    applicant: new FormControl<string>(''),
+    respondent: new FormControl<string>(''),
+    postcode: new FormControl<string>('', {
+      validators: [Validators.pattern(UK_POSTCODE_REGEX)],
+    }),
+    title: new FormControl<string>(''),
+    fee: new FormControl<string>(''),
+    resulted: new FormControl<string>(''),
+  });
+
+  clearSearch(): void {
+    this.form.reset({
+      sequenceNumber: '',
+      accountNumber: '',
+      applicant: '',
+      respondent: '',
+      postcode: '',
+      title: '',
+      fee: '',
+      resulted: '',
+    });
+  }
+
+  onSearch(): void {}
 }
