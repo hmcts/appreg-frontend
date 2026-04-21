58ef362238d307e39d05a80c077caa28222f121b
Preksha Jain <preksha.jain1@hmcts.net>
2026-04-21 14:22:11 +0100

    Lint fixes

--- cypress/support/pageobjects/generic/button/ButtonElement.ts
+++ cypress/support/pageobjects/generic/button/ButtonElement.ts
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
 export class ButtonElement {
-  static findButton(name: string, timeout?: number): Cypress.Chainable<JQuery<HTMLElement>> {
+  static findButton(
+    name: string,
+    timeout?: number,
+  ): Cypress.Chainable<JQuery<HTMLElement>> {
     return cy.contains(
       'button, input[type="button"], input[type="submit"], [role="button"]',
       name,
