315c693be0ddc3919cb3fc0d372aa0ded951d639
Jason Nghiem <Jason.Nghiem@HMCTS.NET>
2026-05-07 14:29:18 +0100

    Delete unused files, add unit tests for SQ marked at 0% coverage

--- src/app/shared/constants/err-msgs.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Error messages
- */
-
-export const phoneIncorrectFormatMsg =
-  'Enter a phone/mobile number in the correct format';
-
-export const emailIncorrectFormatMsg =
-  'Enter an email address in the correct format, like name@example.com';
