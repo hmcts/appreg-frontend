8eaaef180a015f2a00db41aff742ea56468b33a9
Preksha Jain <preksha.jain1@hmcts.net>
2026-05-15 11:37:39 +0100

    @ARCPOC-632 @ARCPOC-821

--- cypress/e2e/step_definitions/elements/upload/WhenFileUploadSteps.ts
+++ cypress/e2e/step_definitions/elements/upload/WhenFileUploadSteps.ts
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 import { When } from '@badeball/cypress-cucumber-preprocessor';
 
+import { UploadHelper } from '../../../../support/helper/forms/upload/UploadHelper';
 import { TestDataGenerator } from '../../../../support/utils/TestDataGenerator';
 
 /**
@@ -18,12 +19,10 @@ When('User Uploads The File {string}', (fileName: string) => {
     const suffix = TestDataGenerator.replaceRandomPlaceholders('{RANDOM}');
     cy.task('generateBulkUploadCsv', { suffix });
   }
-  cy.get('input[type="file"]').selectFile(`cypress/fixtures/${fileName}`, {
-    force: true,
-  });
+  UploadHelper.selectFile(fileName);
   cy.screenshot(`uploaded-file-${fileName}`);
 });
 
 When('User Waits For The File Upload To Complete', () => {
-  cy.get('app-loading-spinner', { timeout: 60000 }).should('not.exist');
+  UploadHelper.waitForBulkUploadToComplete();
 });
