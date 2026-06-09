1be603da920124cda35883220aa9ab7a7aacd73b
jason nghiem hmcts <Jason.Nghiem@HMCTS.NET>
2026-06-09 14:43:40 +0100

    error summary addition

--- src/app/pages/applications/applications-result-selected/applications-result-selected.component.ts
+++ src/app/pages/applications/applications-result-selected/applications-result-selected.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
+import { BreadcrumbsComponent } from '@components/breadcrumbs/breadcrumbs.component';
+import {
+  ErrorItem,
+  ErrorSummaryComponent,
+} from '@components/error-summary/error-summary.component';
+import { SuccessBannerComponent } from '@components/success-banner/success-banner.component';
+import { SuccessBanner } from '@core-types/banner/banner.types';
+import { onCreateErrorClick as onCreateErrorClickFn } from '@util/error-click';
 
 @Component({
   selector: 'app-applications-result-selected',
-  imports: [],
+  imports: [
+    BreadcrumbsComponent,
+    ErrorSummaryComponent,
+    SuccessBannerComponent,
+  ],
   templateUrl: './applications-result-selected.component.html',
   styleUrl: './applications-result-selected.component.scss',
 })
-export class ApplicationsResultSelectedComponent {}
+export class ApplicationsResultSelectedComponent {
+  successBanner = signal<SuccessBanner | null>(null);
+
+  errorSummaryItems = signal<ErrorItem[]>([]);
+  errorFound = computed(() => this.errorSummaryItems().length > 0);
+
+  isSubmitting = signal(false);
+
+  onCreateErrorClick = onCreateErrorClickFn;
+}
