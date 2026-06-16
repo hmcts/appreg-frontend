84ca365b2e9413058caf80bfaf06afd3435e7e44
jason nghiem hmcts <Jason.Nghiem@HMCTS.NET>
2026-06-16 16:07:48 +0100

    test

--- test/unit/app/pages/standard-applicants/standard-applicants-view/standard-applicants-view.component.spec.ts
+++ test/unit/app/pages/standard-applicants/standard-applicants-view/standard-applicants-view.component.spec.ts
@@ -1,14 +1,60 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ActivatedRoute,
+  convertToParamMap,
+  provideRouter,
+} from '@angular/router';
+import { of } from 'rxjs';
 
 import { StandardApplicantsViewComponent } from '@components/standard-applicants/standard-applicants-view/standard-applicants-view.component';
+import { StandardApplicantsApi } from '@openapi';
 
 describe('StandardApplicantsViewComponent', () => {
   let component: StandardApplicantsViewComponent;
   let fixture: ComponentFixture<StandardApplicantsViewComponent>;
 
+  const apiStub: Pick<StandardApplicantsApi, 'getStandardApplicantByCode'> = {
+    getStandardApplicantByCode: jest.fn().mockReturnValue(
+      of({
+        code: 'SA01',
+        name: 'Applicant Org',
+        applicant: {
+          organisation: {
+            name: 'Applicant Org',
+            contactDetails: {
+              addressLine1: '1 Test Street',
+              addressLine2: null,
+              addressLine3: null,
+              addressLine4: null,
+              addressLine5: null,
+              postcode: 'M1 1AA',
+              phone: null,
+              mobile: null,
+              email: null,
+            },
+          },
+        },
+        startDate: '2026-01-01',
+        endDate: null,
+      }),
+    ) as StandardApplicantsApi['getStandardApplicantByCode'],
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [StandardApplicantsViewComponent],
+      providers: [
+        provideRouter([]),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({ id: 'SA01' }),
+            },
+          },
+        },
+        { provide: StandardApplicantsApi, useValue: apiStub },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(StandardApplicantsViewComponent);
