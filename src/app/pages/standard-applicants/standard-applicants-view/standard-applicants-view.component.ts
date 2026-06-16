753d8f3443608445aa474b288d37efc8f2ec988c
jason nghiem hmcts <Jason.Nghiem@HMCTS.NET>
2026-06-16 13:06:31 +0100

    component: read code and send GET. Map response to summary list values

--- src/app/pages/standard-applicants/standard-applicants-view/standard-applicants-view.component.ts
+++ src/app/pages/standard-applicants/standard-applicants-view/standard-applicants-view.component.ts
@@ -1,9 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { BreadcrumbsComponent } from '@components/breadcrumbs/breadcrumbs.component';
+import { StandardApplicantGetDetailDto, StandardApplicantsApi } from '@openapi';
+import { getProblemText } from '@util/http-error-to-text';
+import { formatDate } from '@util/standard-applicant-helpers';
+import { formatPartyName, returnOrgName } from '@util/string-helpers';
+
+interface StandardApplicantSummaryListValues {
+  standardApplicantName?: string | null;
+  addressLine1?: string | null;
+  addressLine2?: string | null;
+  addressLine3?: string | null;
+  addressLine4?: string | null;
+  addressLine5?: string | null;
+  postcode?: string | null;
+  telephoneNumber?: string | null;
+  mobileNumber?: string | null;
+  emailAddress?: string | null;
+  useFrom?: string;
+  useTo?: string | null;
+}
 
 @Component({
   selector: 'app-standard-applicants-view',
-  imports: [],
+  standalone: true,
+  imports: [BreadcrumbsComponent],
   templateUrl: './standard-applicants-view.component.html',
-  styleUrl: './standard-applicants-view.component.scss',
 })
-export class StandardApplicantsViewComponent {}
+export class StandardApplicantsViewComponent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  private readonly saApi = inject(StandardApplicantsApi);
+
+  code = signal('');
+  summaryListValues: StandardApplicantSummaryListValues = {};
+
+  ngOnInit(): void {
+    const code = this.route.snapshot.paramMap.get('id') ?? undefined;
+
+    if (!code) {
+      void this.router.navigate(['/standard-applicants']);
+      return;
+    }
+
+    this.code.set(code);
+
+    this.saApi.getStandardApplicantByCode({ code: this.code() }).subscribe({
+      next: (response) => {
+        this.summaryListValues = this.mapResponseToSummaryList(response);
+      },
+      error: (err) => {
+        const errMsg = getProblemText(err);
+
+        void this.router.navigate(['/standard-applicants'], {
+          queryParams: {
+            applicantDetailFailedToLoad: 'error',
+          },
+          state: {
+            loadError: errMsg,
+          },
+        });
+      },
+    });
+  }
+
+  private mapResponseToSummaryList(
+    data: StandardApplicantGetDetailDto,
+  ): StandardApplicantSummaryListValues {
+    const contactDetails =
+      data.applicant?.organisation?.contactDetails ??
+      data.applicant?.person?.contactDetails;
+
+    return {
+      standardApplicantName:
+        data.name ??
+        returnOrgName(data.applicant) ??
+        formatPartyName(data.applicant) ??
+        '—',
+      addressLine1: contactDetails?.addressLine1 ?? '—',
+      addressLine2: contactDetails?.addressLine2 ?? '—',
+      addressLine3: contactDetails?.addressLine3 ?? '—',
+      addressLine4: contactDetails?.addressLine4 ?? '—',
+      addressLine5: contactDetails?.addressLine5 ?? '—',
+      postcode: contactDetails?.postcode ?? '—',
+      telephoneNumber: contactDetails?.phone ?? '—',
+      mobileNumber: contactDetails?.mobile ?? '—',
+      emailAddress: contactDetails?.email ?? '—',
+      useFrom: formatDate(data.startDate),
+      useTo: formatDate(data.endDate),
+    };
+  }
+}
