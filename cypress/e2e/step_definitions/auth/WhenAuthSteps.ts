1f59e1c0eaf247b0419bea6956233f91586b1181
Phil Head - CGI <152269503+philhead1@users.noreply.github.com>
2026-09-03 10:03:40 +0100

    Added validation of session

--- cypress/e2e/step_definitions/auth/WhenAuthSteps.ts
+++ cypress/e2e/step_definitions/auth/WhenAuthSteps.ts
@@ -1,30 +1,59 @@
 import { When } from '@badeball/cypress-cucumber-preprocessor';
 
-import { APP_URLS } from '../../../support/constants/ProjectConstants';
+import {
+  APP_URLS,
+  AUTH_CONSTANTS,
+  HTTP_CONSTANTS,
+} from '../../../support/constants/ProjectConstants';
 import { AuthErrorScenarios } from '../../../support/helper/auth/AuthErrorScenarios';
 import { AuthHelper } from '../../../support/helper/auth/AuthHelper';
 import { NavigationHelper } from '../../../support/helper/navigation/NavigationHelper';
 import { resolveParallelUserKey } from '../../../support/utils/ParallelUserResolver';
 
-When('User Signs In With Microsoft SSO As {string}', (userType: string) => {
-  cy.session(userType, () => {
-    cy.task<Record<string, { email: string; password: string }>>(
-      'getEnv',
-      'SSO_USERS',
-    ).then((ssoUsers) => {
-      const resolvedUserKey = resolveParallelUserKey(userType, ssoUsers);
-      const user = ssoUsers[resolvedUserKey] || ssoUsers['default'];
-      if (!user) {
-        throw new Error(
-          `SSO user type "${resolvedUserKey}" not found in configuration`,
-        );
-      }
-      NavigationHelper.navigateToPortalPage();
-      AuthHelper.signInWithMicrosoftSSO(user.email, user.password);
-      cy.screenshot(`SSOLogin-${resolvedUserKey}`);
-    });
-    cy.visit(APP_URLS.HOME);
+type SsoUsers = Record<string, { email: string; password: string }>;
+
+const signInWithConfiguredUser = (userType: string): void => {
+  cy.task<SsoUsers>('getEnv', 'SSO_USERS').then((ssoUsers) => {
+    const resolvedUserKey = resolveParallelUserKey(userType, ssoUsers);
+    const user = ssoUsers[resolvedUserKey] || ssoUsers['default'];
+    if (!user) {
+      throw new Error(
+        `SSO user type "${resolvedUserKey}" not found in configuration`,
+      );
+    }
+    NavigationHelper.navigateToPortalPage();
+    AuthHelper.signInWithMicrosoftSSO(user.email, user.password);
+    cy.screenshot(`SSOLogin-${resolvedUserKey}`);
   });
+};
+
+const validateSession = (): void => {
+  cy.request({
+    method: 'GET',
+    url: AUTH_CONSTANTS.SESSION_ENDPOINT,
+    failOnStatusCode: false,
+  }).then((response) => {
+    expect(response.status).to.eq(HTTP_CONSTANTS.STATUS_OK);
+    expect(response.body).to.have.property(
+      AUTH_CONSTANTS.AUTHENTICATED_PROPERTY,
+      AUTH_CONSTANTS.AUTHENTICATED_VALUE,
+    );
+  });
+
+  cy.getCookie(AUTH_CONSTANTS.SESSION_COOKIE_NAME).should('exist');
+};
+
+When('User Signs In With Microsoft SSO As {string}', (userType: string) => {
+  cy.session(
+    userType,
+    () => {
+      signInWithConfiguredUser(userType);
+    },
+    {
+      validate: validateSession,
+    },
+  );
+  cy.visit(APP_URLS.HOME);
 });
 
 When(
