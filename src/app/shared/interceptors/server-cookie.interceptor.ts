855ca615a369a84170bbeb49a26e4663f0c13d12
jason nghiem hmcts <Jason.Nghiem@HMCTS.NET>
2026-09-21 09:09:41 +0100

    read cookie as web request instead of express request

--- src/app/shared/interceptors/server-cookie.interceptor.ts
+++ src/app/shared/interceptors/server-cookie.interceptor.ts
@@ -1,11 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { REQUEST, inject } from '@angular/core';
-import type { Request as ExpressRequest } from 'express';
 
 export const serverCookieInterceptor: HttpInterceptorFn = (req, next) => {
-  const expressReq = inject(REQUEST, { optional: true }) as unknown as
-    | ExpressRequest
-    | undefined;
-  const cookie = expressReq?.headers?.cookie;
+  const request = inject(REQUEST, { optional: true });
+  const cookie = request?.headers.get('cookie');
+
   return next(cookie ? req.clone({ setHeaders: { cookie } }) : req);
 };
