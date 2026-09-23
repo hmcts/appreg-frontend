e31360f6ba413a12b37b66fa9245bff2557c84c5
Tvisha <tvisha.pandya1@hmcts.com>
2026-09-23 11:12:01 +0100

    yarn lint fix ran

--- cypress/e2e/step_definitions/elements/textarea/ThenTextareaSteps.ts
+++ cypress/e2e/step_definitions/elements/textarea/ThenTextareaSteps.ts
@@ -5,21 +5,27 @@ import { TextareaHelper } from '../../../../support/helper/forms/textarea/Textar
 import { TestDataGenerator } from '../../../../support/utils/TestDataGenerator';
 
 Then('User Should See The Textarea {string}', (field: string) => {
-    TextareaHelper.verifyTextareaIsVisible(field);  
-})
+  TextareaHelper.verifyTextareaIsVisible(field);
+});
 
-Then('User Enters {string} Into The {string} Textarea', (value: string, field: string) => {
+Then(
+  'User Enters {string} Into The {string} Textarea',
+  (value: string, field: string) => {
     const substituteValue = TestDataGenerator.replaceRandomPlaceholders(value);
     TextareaHelper.typeInTextarea(field, substituteValue);
     cy.screenshot(`TypedInTextarea-${field}-${substituteValue}`);
-})
+  },
+);
 
 Then('User Clears The {string} Textarea', (field: string) => {
-    TextareaHelper.clearTextarea(field);
-    cy.screenshot(`ClearedTextarea-${field}`);
-})
+  TextareaHelper.clearTextarea(field);
+  cy.screenshot(`ClearedTextarea-${field}`);
+});
 
-Then('User Verifies The {string} Textarea Has Value {string}', (field: string, value: string) => {
+Then(
+  'User Verifies The {string} Textarea Has Value {string}',
+  (field: string, value: string) => {
     const substituteValue = TestDataGenerator.replaceRandomPlaceholders(value);
     TextareaHelper.verifyValueInTextarea(field, substituteValue);
-})
+  },
+);
