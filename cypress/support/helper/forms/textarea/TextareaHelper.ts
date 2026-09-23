856a53c663a530da0dcbef30ed55e32e6b6feaab
Tvisha <tvisha.pandya1@hmcts.com>
2026-09-23 10:53:14 +0100

    ARCPOC-801 AL Create for Description Text Are

---
+++ cypress/support/helper/forms/textarea/TextareaHelper.ts
@@ -0,0 +1,39 @@
+import { TextareaElement } from '../../../pageobjects/textarea/TextareaElement';
+
+export class TextareaHelper {
+    static clearTextarea(selector: string): void {
+        TextareaElement.findTextarea(selector)
+            .should('be.visible')
+            .should('be.enabled')
+            .scrollIntoView()
+            .clear();
+    }
+
+    static typeInTextarea(selector: string, value: string): void {
+        TextareaElement.findTextarea(selector)
+            .should('be.visible')
+            .should('be.enabled')
+            .scrollIntoView()
+            .clear()
+            .type(value, {delay: 0});
+    }
+
+    static verifyTextareaIsVisible(selector: string): void {
+        TextareaElement.findTextarea(selector)
+            .should('be.visible')
+            .scrollIntoView();
+    }
+
+    static verifyValueInTextarea(selector: string, expectedValue: string): void {
+        TextareaElement.findTextarea(selector)
+            .should('be.visible')
+            .scrollIntoView()
+            .invoke('val')
+            .then((actualValue) => {
+                const normalizedActualValue = actualValue ? actualValue.toString().trim() : '';
+                const normalizedExpectedValue = expectedValue.trim();
+                expect(normalizedActualValue).to.equal(normalizedExpectedValue);
+            });
+    }
+}
+    
